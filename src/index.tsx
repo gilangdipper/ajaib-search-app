fc197f3a38dd614051f75d478bd3aea4182acbe7
Gilang Al Khansa <gilang.khansa@prodiginow.com>
2022-02-12 08:52:04 +0700

    add query client

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={new QueryClient()}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 )
