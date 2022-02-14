ed90e19aa4736328fd78ae7d92e71a2c8e3b75c5
Gilang Al Khansa <gilang.khansa@prodiginow.com>
2022-02-14 08:30:05 +0700

    add react-snap

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,23 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { hydrate, render } from 'react-dom'
 import './index.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(
+const rootElement = document.getElementById('root')
+const app = (
   <React.StrictMode>
     <QueryClientProvider client={new QueryClient()}>
       <App />
     </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </React.StrictMode>
 )
+if (rootElement && rootElement.hasChildNodes()) {
+  hydrate(app, rootElement)
+} else {
+  render(app, rootElement)
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
